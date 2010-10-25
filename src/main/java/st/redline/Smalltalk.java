b8f315b7596aade8096ae046bee877c1ac001fea
James Ladd <james_ladd@hotmail.com>
2010-10-25 12:51:35 +1100

    Mapping classname to both Object class and primitive class.

--- src/main/java/st/redline/Smalltalk.java
+++ src/main/java/st/redline/Smalltalk.java
@@ -5,15 +5,22 @@ import java.util.Map;
 
 public class Smalltalk {
 
-	private static Map<String, ProtoObject> classes = new HashMap<String, ProtoObject>();
+	private static Map<String, ProtoObject> classesByName = new HashMap<String, ProtoObject>();
+	private static Map<java.lang.Class, ProtoObject> classesByPrimitiveClass = new HashMap<java.lang.Class, ProtoObject>();
 
 	public static void register(java.lang.Class primitiveClass, ProtoObject objectClass) {
 		String name = primitiveClass.getName().substring(primitiveClass.getName().lastIndexOf('.')+1);
 		System.out.println("Registering " + name + " as " + objectClass);
-		classes.put(name, objectClass);
+		classesByName.put(name, objectClass);
+		classesByPrimitiveClass.put(primitiveClass, objectClass);
 	}
 
 	public static st.redline.ProtoObject classNamed(String className) {
-		return classes.get(className);
+		return classesByName.get(className);
+	}
+
+	public static st.redline.ProtoObject classForPrimitiveClass(java.lang.Class primitiveClass) {
+		System.out.println("Getting class for the primitive Class: " + primitiveClass);
+		return classesByPrimitiveClass.get(primitiveClass);
 	}
 }
