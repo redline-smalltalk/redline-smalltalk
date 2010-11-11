c1afa8387704b938797cba96999cfe6421f81c1f
James Ladd <ladd.james@gmail.com>
2010-11-11 19:19:01 +1100

    Serve now creating instance of requested class. Need to look at design of dispath.

--- src/main/java/st/redline/Serve.java
+++ src/main/java/st/redline/Serve.java
@@ -16,24 +16,47 @@ import org.mortbay.jetty.handler.AbstractHandler;
 
 public class Serve {
 
+	private static final String DEFAULT_OBJECT_TO_SERVE = "Serve";
+
 	// App will be available on localhost:8080
 	public static void main(String[] args) throws Exception {
+		String className = (args.length != 0) ? args[0] : DEFAULT_OBJECT_TO_SERVE;
+
 		Server server = new Server();
 		Connector connector=new SocketConnector();
 		connector.setPort(8080);
 		server.setConnectors(new Connector[]{connector});
 
-		server.setHandler(handler());
+		server.setHandler(handle(instanceOf(className)));
 		server.start();
 		server.join();
 	}
 
-	private static Handler handler() {
+	private static ProtoObject instanceOf(String className) {
+		tryInitializeClass(className);
+		ProtoObject aClass = Smalltalk.classNamed(className);
+		if (aClass != null)
+			return aClass.$send("new");
+		return null;
+	}
+
+	private static void tryInitializeClass(String className) {
+		try {
+			Class.forName("st.redline." + className);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Handler handle(final ProtoObject anObject) {
+		System.out.println("handle(" + anObject + ")");
 		return new AbstractHandler() {
 			public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) throws IOException, ServletException {
 				System.out.println(request);
 				Request base_request = (request instanceof Request) ? (Request) request:HttpConnection.getCurrentConnection().getRequest();
 				base_request.setHandled(true);
+
+				// TODO.jcl Handle dispatch of requests to Object (anObject).
 
 				response.setContentType("text/html");
 				response.setStatus(HttpServletResponse.SC_OK);
