d477ab26fa876deb968e7449b7e43b165684a91a
James Ladd <james_ladd@hotmail.com>
2010-11-22 16:08:00 +1100

    Fleshing out Block support.

---
+++ src/main/java/st/redline/Block.java
@@ -0,0 +1,19 @@
+package st.redline;
+
+public class Block extends ProtoObject {
+
+	@Override
+	public ProtoObject $class() {
+		return null;
+	}
+
+	@Override
+	public ProtoObject $new() {
+		return null;
+	}
+
+	@Override
+	public ProtoObject $fromPrimitive(Object value) {
+		return null;
+	}
+}
