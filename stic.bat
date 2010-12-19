fb0e915252a9617eba3a6106c48ca189d7696945
James Ladd <james_ladd@hotmail.com>
2010-12-20 08:38:43 +1100

    Added stic.bat for windows.

---
+++ stic.bat
@@ -0,0 +1,6 @@
+@echo off
+SetLocal EnableDelayedExpansion
+set the_classpath=out\redline-smalltalk.jar;
+for %%i in (lib\*.jar) do set the_classpath=!the_classpath!%%i;
+java -cp "%the_classpath%" st.redline.smalltalk.Stic %*
+EndLocal
