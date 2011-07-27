3b360a2ab549cdce11e7c7a453c73e49ce47f731
James Ladd <james.ladd@iplayup.com>
2011-07-28 08:32:47 +1000

    Modified traceoutput.

--- src/main/java/st/redline/bootstrap/ClassSubclassMethod.java
+++ src/main/java/st/redline/bootstrap/ClassSubclassMethod.java
@@ -5,7 +5,7 @@ import st.redline.ProtoObject;
 
 public class ClassSubclassMethod extends ProtoMethod {
 	public ProtoObject applyTo(ProtoObject receiver, ProtoObject classMethodWasFoundIn, ProtoObject argument) {
-		System.out.println("ClassSubclassMethod applyTo() " + argument.javaValue());
+		System.out.println("ClassSubclassMethod applyTo() " + String.valueOf(argument.javaValue()));
 		return null;
 	}
 }
