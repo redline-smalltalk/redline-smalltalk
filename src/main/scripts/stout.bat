8de3d2bec4844964823482ebf9d64ca5468f2f71
Robert Roland <rob.roland@gmail.com>
2011-09-02 22:22:35 -0700

    Adding Maven support
    Builds a distributable version of the project in target/redline-deploy

--- src/main/scripts/stout.bat
+++ src/main/scripts/stout.bat
@@ -1,6 +1,5 @@
 @echo off
 SetLocal EnableDelayedExpansion
-set the_classpath=out\redline-smalltalk.jar;
 for %%i in (lib\*.jar) do set the_classpath=!the_classpath!%%i;
 java -cp "%the_classpath%" st.redline.stout.Run %*
 EndLocal
