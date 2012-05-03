af0ebb450ad53f91dcaceb914e3724f252942b8d
James Ladd <ladd.james@gmail.com>
2012-05-03 17:08:36 +1000

    Added ground work for exposing the compiler into smalltalk for direct string evaluation.

--- src/main/java/st/redline/core/PrimObject.java
+++ src/main/java/st/redline/core/PrimObject.java
@@ -98,6 +98,9 @@ public class PrimObject {
         return this;
     }
 
+    public void sendMessagesResult(PrimObject result) {
+    }
+
     public PrimObject block(String name, PrimContext context) {
         // NOTE: When asking a keyword expression is it contains a block with an answer expression,
         // we should also take that opportunity to mark the block node as being a method block or
