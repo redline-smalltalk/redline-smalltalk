3ff57b76744a279b2f12528f5152749d2ff0038a
James Ladd <ladd.james@gmail.com>
2012-06-07 12:55:50 +1000

    Working my way towards having import:as: working and automatically adapting to java classes.

---
+++ src/main/java/st/redline/core/ArgThing.java
@@ -0,0 +1,9 @@
+package st.redline.core;
+
+public class ArgThing {
+
+    public ArgThing(int i) {
+        System.out.println("****************");
+        System.out.println("ArgThing constructor called with: " + i);
+    }
+}
